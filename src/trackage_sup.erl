938ce2c92da9fbab7e909fe23089b3489c5efe1a
Zach Peterson <zachpeterson13@icloud.com>
2023-03-22 15:55:02 -0600

    feat: implemented store_facility_sup and added to root_sup

--- src/trackage_sup.erl
+++ src/trackage_sup.erl
@@ -31,11 +31,12 @@ init([]) ->
       period => 1},
 
   Sp_sup = generate_spec(store_package_sup, supervisor, store_package_sup, []),
+  Sf_sup = generate_spec(store_facility_sup, supervisor, store_facility_sup, []),
   Gp_sup = generate_spec(get_package_sup, supervisor, get_package_sup, []),
   Gv_sup = generate_spec(get_vehicle_sup, supervisor, get_vehicle_sup, []),
   Gf_sup = generate_spec(get_facility_sup, supervisor, get_facility_sup, []),
 
-  ChildSpecs = [Sp_sup, Gp_sup, Gv_sup, Gf_sup],
+  ChildSpecs = [Sp_sup, Sf_sup, Gp_sup, Gv_sup, Gf_sup],
   {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
