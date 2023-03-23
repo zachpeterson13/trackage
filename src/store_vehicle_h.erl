ddaee09986c41d92b7dad574025bb7cd1f241678
Zach Peterson <zachpeterson13@icloud.com>
2023-03-23 10:00:01 -0600

    feat: implemented store_vehicle handler

---
+++ src/store_vehicle_h.erl
@@ -0,0 +1,26 @@
+%% @doc A handler to store the package data in the database.
+-module(store_vehicle_h).
+
+-export([init/2]).
+
+init(Req0, Opts) ->
+  {ok, Data, _} = cowboy_req:read_body(Req0),
+
+  #{<<"vehicle_uuid">> := Veh,
+    <<"location">> := Location,
+    <<"time_stamp">> := Timestamp} =
+    jsx:decode(Data),
+
+  Vehicle_uuid = binary_to_list(Veh),
+  #{<<"lat">> := Lat, <<"long">> := Long} = Location,
+
+  List =
+    get_vehicle:get(
+      rr_distributor:get(get_vehicle_rr), Vehicle_uuid),
+
+  store_package:store(
+    rr_distributor:get(store_package_rr), Vehicle_uuid, [{Lat, Long, Timestamp} | List]),
+
+  Req = cowboy_req:reply(200, #{<<"content-type">> => <<"text/json">>}, "[\"done\"]", Req0),
+
+  {ok, Req, Opts}.
--- src/trackage_app.erl
+++ src/trackage_app.erl
@@ -13,6 +13,7 @@ start(_StartType, _StartArgs) ->
   Dispatch =
     cowboy_router:compile([{'_',
                             [{"/store_package_info", store_package_h, []},
+                             {"/store_vehicle_info", store_vehicle_h, []},
                              {"/query_package_history", get_package_h, []},
                              {"/query_vehicle_history", get_vehicle_h, []},
                              {"/query_facility", get_facility_h, []}]}]),
